fe04e4040afa0557908b03c3d7d08a136567242f
Małgorzata Klisińska <klis.mal@gmail.com>
2017-10-22 13:51:43 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -32,3 +32,7 @@ function removeElementFromBeginningOfArray(array){
   var newArray = array.slice(1)
   return newArray
 }
+function destructivelyRemoveElementFromEndOfArray(array){
+  array.pop()
+  return array
+}
